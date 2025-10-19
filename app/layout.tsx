6df108d2f3b6a88b95fd00a702f4de477004fb8e
atakhatri <atakhatri905@gmail.com>
2025-10-19 18:49:21 +0530

    Push to Production

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ import Navbar from "../components/layout/navbar";
 import Footer from "../components/layout/footer";
 import BackToTopButton from "../components/ui/back-to-top";
 import { ThemeProvider } from "../components/providers/theme-provider";
-import LightRays from "../components/providers/lightrays";
 
 const poppins = Poppins({
   subsets: ["latin"],
